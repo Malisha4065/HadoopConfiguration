e2598480ce56b3454fb548c4fe16ea3960f03c39
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-06-05 20:02:44 +0530

    update initial script files

--- initial.bat
+++ initial.bat
@@ -6,9 +6,11 @@ REM Build the hadoop-client image
 docker compose build hadoop-client
 
 REM Change ownership of the namenode data directory
+docker compose run --rm --user root namenode rm -rf /opt/hadoop/dfs/name/*
 docker compose run --rm --user root namenode chown -R 1000:1000 /opt/hadoop/dfs/name
+docker compose run --rm namenode hdfs namenode -format -force
 
-REM Change ownership of the datanode data directory
+docker compose run --rm --user root datanode rm -rf /opt/hadoop/dfs/data/*
 docker compose run --rm --user root datanode chown -R 1000:1000 /opt/hadoop/dfs/data
 
 REM Start the containers
